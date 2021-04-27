182f25a78cc8e99da46d242970bd97cc6759bd2c
P.Y. Cheng <cs2world@gmail.com>
2021-04-26 22:42:02 -0700

    kept only deposit in this version, removed all others.

--- include/depositnlock.hpp
+++ include/depositnlock.hpp
@@ -11,7 +11,7 @@ CONTRACT depositnlock : public contract
 public:
    using contract::contract;
 
-   static constexpr symbol ant_symbol = symbol("DFS", 2);
+   static constexpr symbol ant_symbol = symbol("ANT", 2);
 
    static constexpr uint32_t seconds_per_day = 1;
    static constexpr uint32_t refund_delay_sec = 3 * seconds_per_day;
@@ -22,13 +22,11 @@ public:
    {
    }
 
-   ACTION undeposit(name user, asset quantity);
-
+   
    [[eosio::on_notify("tokenfactory::transfer")]] 
    void deposit(name from, name to, asset quantity, std::string memo);
 
-   ACTION withdraw(name user, uint64_t id);
-
+   
 private:
    TABLE deposit_st
    {
@@ -38,16 +36,7 @@ private:
       uint64_t primary_key() const { return owner.value; }
    };
 
-   TABLE withdraw_st
-   {
-      uint64_t id;
-      asset withdraw_amount;
-      time_point_sec request_time;
-      uint64_t primary_key() const { return id; }
-   };
-
    typedef multi_index<name("deposits"), deposit_st> deposits;
    deposits _deposits;
 
-   typedef multi_index<name("withdraws"), withdraw_st> withdraws;
 };
--- src/depositnlock.cpp
+++ src/depositnlock.cpp
@@ -1,44 +1,5 @@
 #include <depositnlock.hpp>
 
-ACTION depositnlock::undeposit(name user, asset quantity)
-{
-   require_auth(user);
-
-   auto itr = _deposits.require_find(user.value, "deposit not found");
-   check(quantity <= itr->balance, "invalid quantity");
-
-   _deposits.modify(itr, same_payer, [&](auto &s) {
-      s.balance -= quantity;
-   });
-
-   if (itr->balance.amount == 0)
-   {
-      _deposits.erase(itr);
-   }
-
-   
-   action{permission_level{get_self(), name("active")}, name("tokenfactory"), name("transfer"),
-          make_tuple(get_self(), user, quantity, std::string("deposit withdraw..."))}
-       .send();
-}
-
-ACTION depositnlock::withdraw(name user, uint64_t id)
-{
-   // require_auth(user);
-
-   withdraws _withdraws(get_self(), user.value);
-
-   auto req = _withdraws.require_find(id, "refund request not found");
-
-   check(req->request_time + seconds(refund_delay_sec) <= current_time_point(),
-         "refund is not available yet");
-
-   action{permission_level{get_self(), name("active") }, name("tokenfactory"), name("transfer"),
-          make_tuple(get_self(), user, req->withdraw_amount, std::string("deposit refund"))}
-       .send();
-
-   _withdraws.erase(req);
-}
 
 void depositnlock::deposit(name from, name to, asset quantity, std::string memo)
 {
